a8935706af9ed379fa8d1f1cb17e7b21e4c88342
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-23 18:39:06 -0400

    replace tabs with spaces

--- csrand.rb
+++ csrand.rb
@@ -5,110 +5,110 @@ MBIG = INT_MAX
 MSEED = 161803398
 
 class CSRandom
-	attr_accessor :inext, :inextp, :seed_array
+  attr_accessor :inext, :inextp, :seed_array
 
-	def initialize(seed)
-		@seed_array = []
+  def initialize(seed)
+    @seed_array = []
 
-	  subtraction = (seed == INT_MIN) ? INT_MAX : seed.abs
-	  mj = MSEED - subtraction
-	  seed_array[55] = mj
-	  mk = 1
+    subtraction = (seed == INT_MIN) ? INT_MAX : seed.abs
+    mj = MSEED - subtraction
+    seed_array[55] = mj
+    mk = 1
 
-	  (1..54).each do |i|
-	  	ii = (21 * i) % 55
-	  	seed_array[ii] = mk
-	  	mk = mj - mk
-	  	mk += MBIG if mk < 0
-	  	mj = seed_array[ii]
-	  end
+    (1..54).each do |i|
+      ii = (21 * i) % 55
+      seed_array[ii] = mk
+      mk = mj - mk
+      mk += MBIG if mk < 0
+      mj = seed_array[ii]
+    end
 
-	  (1..4).each do |k|
-	  	(1..55).each do |i|
-	  		seed_array[i] -= seed_array[1 + (i + 30) % 55]
-	  		seed_array[i] += MBIG if seed_array[i] < 0
-	  	end
-	  end
+    (1..4).each do |k|
+      (1..55).each do |i|
+        seed_array[i] -= seed_array[1 + (i + 30) % 55]
+        seed_array[i] += MBIG if seed_array[i] < 0
+      end
+    end
 
-	  @inext = 0
-	  @inextp = 21
-	end
+    @inext = 0
+    @inextp = 21
+  end
 
-	def sample
-		internal_sample * ( 1.0 / MBIG )
-	end
+  def sample
+    internal_sample * ( 1.0 / MBIG )
+  end
 
-	def internal_sample
-		locINext = @inext
-		locINextp = @inextp
+  def internal_sample
+    locINext = @inext
+    locINextp = @inextp
 
-		locINext += 1
-		locINextp += 1
+    locINext += 1
+    locINextp += 1
 
-		if locINext >= 56
-			locINext = 1
-		end
+    if locINext >= 56
+      locINext = 1
+    end
 
-		if locINextp >= 56
-			locINextp = 1 
-		end
+    if locINextp >= 56
+      locINextp = 1 
+    end
 
-		return_value = seed_array[locINext] - seed_array[locINextp]
+    return_value = seed_array[locINext] - seed_array[locINextp]
 
-		if return_value == MBIG
-			return_value -= 1
-		end
+    if return_value == MBIG
+      return_value -= 1
+    end
 
-		if return_value < 0
-			return_value += MBIG
-		end
+    if return_value < 0
+      return_value += MBIG
+    end
 
-		seed_array[locINext] = return_value
+    seed_array[locINext] = return_value
 
-		@inext = locINext
-		@inextp = locINextp
+    @inext = locINext
+    @inextp = locINextp
 
-		return_value
-	end
+    return_value
+  end
 
-	# def get_sample_for_large_range
-	# 	result = internal_sample
-	# 	if internal_sample % 2 === 0
-	# 		result = -result
-	# 	end
+  # def get_sample_for_large_range
+  #   result = internal_sample
+  #   if internal_sample % 2 === 0
+  #     result = -result
+  #   end
 
-	# 	d = result
-	# 	d += (INT_MAX - 1)
-	# 	d /= 2 * INT_MAX - 1
-	# 	d
-	# end
+  #   d = result
+  #   d += (INT_MAX - 1)
+  #   d /= 2 * INT_MAX - 1
+  #   d
+  # end
 
-	def next(*args)
-		a, b = *args
-		min = 0
-		max = INT_MAX
+  def next(*args)
+    a, b = *args
+    min = 0
+    max = INT_MAX
 
-		if b.is_a? Integer
-			raise ArgumentError, "Argument out of range" if a > b
-			max = b
-			min = a if a.is_a? Integer
+    if b.is_a? Integer
+      raise ArgumentError, "Argument out of range" if a > b
+      max = b
+      min = a if a.is_a? Integer
 
-			range = max - min
-			if range <= INT_MAX
-				(sample * range + min).to_i 
-			else
-				#get_sample_for_large_range * range + min
-			end
-		elsif a.is_a? Integer
-			raise ArgumentError, "Argument out of range" if a < 0
-			max = a
-			(sample * max).to_i 
-		else
-			internal_sample
-		end
-	end
+      range = max - min
+      if range <= INT_MAX
+        (sample * range + min).to_i 
+      else
+        #get_sample_for_large_range * range + min
+      end
+    elsif a.is_a? Integer
+      raise ArgumentError, "Argument out of range" if a < 0
+      max = a
+      (sample * max).to_i 
+    else
+      internal_sample
+    end
+  end
 
-	def next_double
-		sample
-	end
+  def next_double
+    sample
+  end
 end
